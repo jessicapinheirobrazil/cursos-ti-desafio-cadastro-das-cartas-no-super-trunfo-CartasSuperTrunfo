07773646409236d81bea1d783baeb203c75f0e90
Jessica Brazil <67717969+jessicapinheirobrazil@users.noreply.github.com>
2025-07-19 04:25:40 +0000

    Implementa o desafio com duas cartas do Super Trunfo (código todo em inglês para treinar)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,65 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// First card
+char state[100], code_card[4], city_name[100];
+int population, number_of_tourist_attractions;
+float area, pib;
+// Second card
+char state_2[100], code_card_2[4], city_name_2[100];
+int population_2, number_of_tourist_attractions_2;
+float area_2, pib_2;
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Let's create the first card of the game Super Trunfo!\n");
+    printf("Follow the instructions below.\n");
+    printf("Type the state: ");
+    scanf("%s", state);
+    printf("\nType the code of the card. The code must be 3 characters long: ");
+    scanf("%s", code_card);
+    printf("\nType the name of the city: ");
+    scanf("%s", city_name);
+    printf("\nType the population of the city: ");
+    scanf("%d", &population);
+    printf("\nType the number of tourist attractions: ");
+    scanf("%d", &number_of_tourist_attractions);
+    printf("Type the area of the city: ");
+    scanf("%f", &area);
+    printf("Type the PIB of the city: ");
+    scanf("%f", &pib);
+
+    printf("\n\nNow, let's create the second card of the game Super Trunfo.\n");
+    printf("Type the state: ");
+    scanf("%s", state_2);
+    printf("\nType the code of the card. Remember that it must be 3 characters long: ");
+    scanf("%s", code_card_2);
+    printf("\nType the name of the city: ");
+    scanf("%s", city_name_2);
+    printf("\nType the population of the city: ");
+    scanf("%d", &population_2);
+    printf("\nType the number of tourist attractions: ");
+    scanf("%d", &number_of_tourist_attractions_2);
+    printf("Type the area of the city: ");
+    scanf("%f", &area_2);
+    printf("Type the PIB of the city: ");
+    scanf("%f", &pib_2);
+
+    printf("\nCard 1:\n");
+    printf("State: %s\n", state);
+    printf("Code: %s\n", code_card);
+    printf("City: %s\n", city_name);
+    printf("Population: %d\n", population);
+    printf("Tourist Attractions: %d\n", number_of_tourist_attractions);
+    printf("Area: %.3f\n", area);
+    printf("PIB: %.2f\n", pib);
+
+    printf("\n\nCard 2:\n");
+    printf("State: %s\n", state_2);
+    printf("Code: %s\n", code_card_2);
+    printf("City: %s\n", city_name_2);
+    printf("Population: %d\n", population_2);
+    printf("Tourist Attractions: %d\n", number_of_tourist_attractions_2);
+    printf("Area: %.3f\n", area_2);
+    printf("PIB: %.2f\n", pib_2);
 
     return 0;
 }
